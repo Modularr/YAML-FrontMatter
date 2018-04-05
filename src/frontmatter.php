e6e0400db5c9ef42989cddecec717c5b915bf30c
modbot <git@modularr.com>
2018-04-05 23:41:15 +0200

    Commit

--- src/frontmatter.php
+++ src/frontmatter.php
@@ -4,7 +4,6 @@ use Symfony\Component\Yaml\Yaml;
  * PHP YAML FrontMatter Class
  * An easy to use class for handling YAML frontmatter in PHP.
  *
- * @author Blaxus
  * @package Modularr/YAML-FrontMatter
  */
 class FrontMatter
@@ -14,7 +13,7 @@ class FrontMatter
      * @param array $data metadata & content
      */
     public $data;
-
+    
     /**
      * Constructor method, checks a file and then puts the contents into custom strings for usage
      * @param string $file The input file
@@ -24,13 +23,12 @@ class FrontMatter
         $file = (file_exists($file)) ? $this->Read($file) : str_replace(array("\r\n", "\r", "\n"), "\n", $file);
         $this->yaml_separator = "---\n";
         $fm = $this->FrontMatter($file);
-
-        foreach($fm as $key => $value)
-        {
+        
+        foreach($fm as $key => $value) {
             $this->data[$key] = $value;
         }
     }
-
+    
     /**
      * fetch method returns the value of a given key
      * @return string $value The value for a given key
@@ -39,7 +37,7 @@ class FrontMatter
     {
         return $this->data[$key];
     }
-
+    
     /**
      * keyExists method Checks to see if a key exists
      * @return bool
@@ -49,7 +47,7 @@ class FrontMatter
         #return (isset($this->data[$key])) ? true : false; # Isset Version
         return array_key_exists($key, $this->data); # array_key_exists version
     }
-
+    
     /**
      * fetchKeys method returns an array of all meta data without the content
      * @return [array] collection of all meta keys provided to FrontMatter
@@ -58,13 +56,13 @@ class FrontMatter
     {
         # Cache the keys so we don't edit the native object data
         $keys = $this->data;
-
+        
         # Remove $data[content] from the keys so we only have the meta data
         array_pop($keys);
-
+        
         return $keys;
     }
-
+    
     /**
      * FrontMatter method, rturns all the variables from a YAML Frontmatter input
      * @param string $input The input string
@@ -72,8 +70,7 @@ class FrontMatter
      */
     function FrontMatter($input)
     {
-        if (!$this->startsWith($input, $this->yaml_separator))
-        {
+        if (!$this->startsWith($input, $this->yaml_separator)) {
             # No front matter
             # Store Content in Final array
             $final['content'] = $input;
@@ -83,9 +80,8 @@ class FrontMatter
 
         # Explode Seperators. At most, make three pieces out of the input file
         $document = explode($this->yaml_separator,$input, 3);
-
-        switch( sizeof($document) )
-        {
+        
+        switch( sizeof($document) ) {
             case 0:
             case 1:
                 // Empty document
@@ -102,21 +98,21 @@ class FrontMatter
                 $front_matter = $document[1];
                 $content = $document[2];
         }
-
+        
         # Parse YAML
 		try {
 			$final = Yaml::parse($front_matter);
 		} catch (ParseException $e) {
 			printf("Unable to parse the YAML string: %s", $e->getMessage());
 		}
-
+        
         # Store Content in Final array
         $final['content'] = $content;
-
+        
         # Return Final array
         return $final;
     }
-
+    
     /**
      * A convenience wrapper around strpos to check the start of a string
      * From http://stackoverflow.com/a/860509/270334
@@ -128,7 +124,7 @@ class FrontMatter
             return strpos($haystack, $needle, 0) === 0;
         return stripos($haystack, $needle, 0) === 0;
     }
-
+    
     /**
      * Read Method, Read file and returns it's contents
      * @return string $data returned data
@@ -137,25 +133,21 @@ class FrontMatter
     {
         # Open File
         $fh = fopen($file, 'r');
-
         $fileSize = filesize($file);
-
-        if(!empty($fileSize))
-        {
+        
+        if(!empty($fileSize)) {
             # Read Data
             $data = fread($fh, $fileSize);
-
+            
             # Fix Data Stream to be the exact same format as PHP's strings
             $data = str_replace(array("\r\n", "\r", "\n"), "\n", $data);
-        }
-        else
-        {
+        } else {
             $data = '';
         }
-
+        
         # Close File
         fclose($fh);
-
+        
         # Return Data
         return $data;
     }
