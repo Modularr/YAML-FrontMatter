fa6386a7c20ba7d1ab00f7723d8437f93e018df0
Peter Thaleikis <peter.thaleikis@gmail.com>
2020-10-09 12:19:53 +0400

    Improving main class

--- src/FrontMatter.php
+++ src/FrontMatter.php
@@ -14,12 +14,14 @@ class FrontMatter
 {
     /**
      * All the parameters.
-     * @param array $data metadata & content
+     *
+     * @var array $data metadata & content
      */
     public $data;
 
     /**
      * Constructor method, checks a file and then puts the contents into custom strings for usage
+     *
      * @param string $file The input file
      */
     public function __construct($file)
@@ -34,7 +36,8 @@ class FrontMatter
     }
 
     /**
-     * fetch method returns the value of a given key
+     * Fetch method returns the value of a given key
+     *
      * @return string $value The value for a given key
      */
     public function fetch($key)
@@ -43,25 +46,26 @@ class FrontMatter
     }
 
     /**
-     * keyExists method Checks to see if a key exists
+     * KeyExists method Checks to see if a key exists
+     *
      * @return bool
      */
     public function keyExists($key)
     {
-        #return (isset($this->data[$key])) ? true : false; # Isset Version
-        return array_key_exists($key, $this->data); # array_key_exists version
+        return (isset($this->data[$key])) ? true : false;
     }
 
     /**
-     * fetchKeys method returns an array of all meta data without the content
+     * FetchKeys method returns an array of all meta data without the content
+     *
      * @return [array] collection of all meta keys provided to FrontMatter
      */
     public function fetchKeys()
     {
-        # Cache the keys so we don't edit the native object data
+        // Cache the keys so we don't edit the native object data
         $keys = $this->data;
 
-        # Remove $data[content] from the keys so we only have the meta data
+        // Remove $data[content] from the keys so we only have the meta data
         array_pop($keys);
 
         return $keys;
@@ -69,20 +73,21 @@ class FrontMatter
 
     /**
      * FrontMatter method, rturns all the variables from a YAML Frontmatter input
+     *
      * @param string $input The input string
      * @return array $final returns all variables in an array
      */
     function FrontMatter($input)
     {
         if (!$this->startsWith($input, $this->yaml_separator)) {
-            # No front matter
-            # Store Content in Final array
+            // No front matter
+            // Store Content in Final array
             $final['content'] = $input;
-            # Return Final array
+            // Return Final array
             return $final;
         }
 
-        # Explode Seperators. At most, make three pieces out of the input file
+        // Explode Seperators. At most, make three pieces out of the input file
         $document = explode($this->yaml_separator,$input, 3);
 
         switch( sizeof($document) ) {
@@ -93,33 +98,34 @@ class FrontMatter
                 $content = "";
                 break;
             case 2:
-                # Only front matter given
+                // Only front matter given
                 $front_matter = $document[1];
                 $content = "";
                 break;
             default:
-                # Normal document
+                // Normal document
                 $front_matter = $document[1];
                 $content = $document[2];
         }
 
-        # Parse YAML
+        // Parse YAML
         try {
             $final = Yaml::parse($front_matter);
         } catch (ParseException $e) {
             printf("Unable to parse the YAML string: %s", $e->getMessage());
         }
 
-        # Store Content in Final array
+        // Store Content in Final array
         $final['content'] = $content;
 
-        # Return Final array
+        // Return Final array
         return $final;
     }
 
     /**
      * A convenience wrapper around strpos to check the start of a string
      * From http://stackoverflow.com/a/860509/270334
+     *
      * @return boolean $startswithneedle string starts with $needle
      */
     private function startsWith($haystack,$needle,$case=true)
@@ -131,28 +137,29 @@ class FrontMatter
 
     /**
      * Read Method, Read file and returns it's contents
+     *
      * @return string $data returned data
      */
     protected function Read($file)
     {
-        # Open File
+        // Open File
         $fh = fopen($file, 'r');
         $fileSize = filesize($file);
 
         if(!empty($fileSize)) {
-            # Read Data
+            // Read Data
             $data = fread($fh, $fileSize);
 
-            # Fix Data Stream to be the exact same format as PHP's strings
+            // Fix Data Stream to be the exact same format as PHP's strings
             $data = str_replace(array("\r\n", "\r", "\n"), "\n", $data);
         } else {
             $data = '';
         }
 
-        # Close File
+        // Close File
         fclose($fh);
 
-        # Return Data
+        // Return Data
         return $data;
     }
 }
