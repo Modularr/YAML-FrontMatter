d0e97458b817a98199c3b75fbcdc27d7b36c88ad
Peter Thaleikis <peter.thaleikis@gmail.com>
2020-10-09 12:15:27 +0400

    PSR-4 classloading, updating deps, namespacing

--- src/FrontMatter.php
+++ src/FrontMatter.php
@@ -1,5 +1,9 @@
 <?php
+
+namespace Modularr\YAMLFrontmatter;
+
 use Symfony\Component\Yaml\Yaml;
+
 /**
  * PHP YAML FrontMatter Class
  * An easy to use class for handling YAML frontmatter in PHP.
@@ -13,7 +17,7 @@ class FrontMatter
      * @param array $data metadata & content
      */
     public $data;
-    
+
     /**
      * Constructor method, checks a file and then puts the contents into custom strings for usage
      * @param string $file The input file
@@ -23,12 +27,12 @@ class FrontMatter
         $file = (file_exists($file)) ? $this->Read($file) : str_replace(array("\r\n", "\r", "\n"), "\n", $file);
         $this->yaml_separator = "---\n";
         $fm = $this->FrontMatter($file);
-        
+
         foreach($fm as $key => $value) {
             $this->data[$key] = $value;
         }
     }
-    
+
     /**
      * fetch method returns the value of a given key
      * @return string $value The value for a given key
@@ -37,7 +41,7 @@ class FrontMatter
     {
         return $this->data[$key];
     }
-    
+
     /**
      * keyExists method Checks to see if a key exists
      * @return bool
@@ -47,7 +51,7 @@ class FrontMatter
         #return (isset($this->data[$key])) ? true : false; # Isset Version
         return array_key_exists($key, $this->data); # array_key_exists version
     }
-    
+
     /**
      * fetchKeys method returns an array of all meta data without the content
      * @return [array] collection of all meta keys provided to FrontMatter
@@ -56,13 +60,13 @@ class FrontMatter
     {
         # Cache the keys so we don't edit the native object data
         $keys = $this->data;
-        
+
         # Remove $data[content] from the keys so we only have the meta data
         array_pop($keys);
-        
+
         return $keys;
     }
-    
+
     /**
      * FrontMatter method, rturns all the variables from a YAML Frontmatter input
      * @param string $input The input string
@@ -80,7 +84,7 @@ class FrontMatter
 
         # Explode Seperators. At most, make three pieces out of the input file
         $document = explode($this->yaml_separator,$input, 3);
-        
+
         switch( sizeof($document) ) {
             case 0:
             case 1:
@@ -98,21 +102,21 @@ class FrontMatter
                 $front_matter = $document[1];
                 $content = $document[2];
         }
-        
+
         # Parse YAML
-		try {
-			$final = Yaml::parse($front_matter);
-		} catch (ParseException $e) {
-			printf("Unable to parse the YAML string: %s", $e->getMessage());
-		}
-        
+        try {
+            $final = Yaml::parse($front_matter);
+        } catch (ParseException $e) {
+            printf("Unable to parse the YAML string: %s", $e->getMessage());
+        }
+
         # Store Content in Final array
         $final['content'] = $content;
-        
+
         # Return Final array
         return $final;
     }
-    
+
     /**
      * A convenience wrapper around strpos to check the start of a string
      * From http://stackoverflow.com/a/860509/270334
@@ -124,7 +128,7 @@ class FrontMatter
             return strpos($haystack, $needle, 0) === 0;
         return stripos($haystack, $needle, 0) === 0;
     }
-    
+
     /**
      * Read Method, Read file and returns it's contents
      * @return string $data returned data
@@ -134,20 +138,20 @@ class FrontMatter
         # Open File
         $fh = fopen($file, 'r');
         $fileSize = filesize($file);
-        
+
         if(!empty($fileSize)) {
             # Read Data
             $data = fread($fh, $fileSize);
-            
+
             # Fix Data Stream to be the exact same format as PHP's strings
             $data = str_replace(array("\r\n", "\r", "\n"), "\n", $data);
         } else {
             $data = '';
         }
-        
+
         # Close File
         fclose($fh);
-        
+
         # Return Data
         return $data;
     }
